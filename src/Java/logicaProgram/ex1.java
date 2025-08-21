43a800e8b5ffc3212ca7c70dac8e806dbd6de3f9
Samuel <henrisamuel234@gmail.com>
2025-08-21 17:10:54 -0300

    EXERCICIO 1

--- src/Java/logicaProgram/ex1.java
+++ src/Java/logicaProgram/ex1.java
@@ -1,7 +1,22 @@
 package Java.logicaProgram;
 
+import java.util.Scanner;
+
 public class ex1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("DIgite um numero inteiro: ");
+        int num = sc.nextInt();
+
+        if (num > 0 ) {
+            System.out.print("POSITIVO");
+        } else if (num < 0) {
+            System.out.print("NEGATIVO");
+        } else {
+            System.out.print("ZERO");
+        }
+
+        sc.close();
     }
 }
