6025c02fe30ea4ba494f35fba0ff1ba88405dc60
Samuel <henrisamuel234@gmail.com>
2025-08-22 17:17:02 -0300

    EXERCICIO 6

---
+++ src/Java/logicaProgram/Ex6.java
@@ -0,0 +1,17 @@
+package Java.logicaProgram;
+
+import java.util.Scanner;
+
+public class Ex6 {
+    public static void main (String []args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite tres numeros inteiros");
+        int num1 = sc.nextInt(), num2 = sc.nextInt(), num3 = sc.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+
+        }
+        sc.close();
+    }
+}
