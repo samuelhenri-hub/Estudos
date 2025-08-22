21d26123eebfa801c5c9d1341943e9dfcc4f8037
Samuel <henrisamuel234@gmail.com>
2025-08-22 16:54:54 -0300

    EXERCICIO 4 terminado

--- src/Java/logicaProgram/Ex4.java
+++ src/Java/logicaProgram/Ex4.java
@@ -1,7 +1,24 @@
 package Java.logicaProgram;
 
+import java.util.Scanner;
+
 public class Ex4 {
     public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite dois numeros inteiros:");
+        int num1 = sc.nextInt(), num2 = sc.nextInt();
+
+        if (num1 > num2) {
+            System.out.printf("%d é maior que %d", num1, num2);
+        }
+        else if (num1 < num2) {
+            System.out.printf("%d é menor que %d", num1, num2);
+        }
+        else {
+            System.out.print("São iguais");
+        }
+
+        sc.close();
     }
 }
