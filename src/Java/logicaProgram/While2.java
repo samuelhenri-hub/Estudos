bdd5c1cdb8169a6424060dcb54df98059e273bdd
Samuel <henrisamuel234@gmail.com>
2025-08-28 18:20:20 -0300

    Exercicio while (medio) inacabado

---
+++ src/Java/logicaProgram/While2.java
@@ -0,0 +1,21 @@
+package Java.logicaProgram;
+
+import java.util.Scanner;
+
+public class While2 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um numero");
+        int num = sc.nextInt();
+        int mais = 0;
+
+        while (num != 0) {
+            int num2 = sc.nextInt();
+            int soma = num + num2;
+        }
+        System.out.print("RESULTADO: " + );
+
+        sc.close();
+    }
+}
