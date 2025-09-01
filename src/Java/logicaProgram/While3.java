b063db0d5dc46eaee50660d506cac2a535d6e4bc
Samuel <henrisamuel234@gmail.com>
2025-09-01 16:32:52 -0300

    EXERCICIO 6 while corrigido

--- src/Java/logicaProgram/While3.java
+++ src/Java/logicaProgram/While3.java
@@ -1,4 +1,28 @@
 package Java.logicaProgram;
 
+import java.util.Scanner;
+
 public class While3 {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int pos = 0;
+        int neg = 0;
+
+        int num = sc.nextInt();
+
+        while (num != 999) {
+            num = sc.nextInt();
+
+            if (num >= 0) {
+                pos += 1;
+            }
+            else {
+                neg += 1;
+            }
+        }
+        System.out.printf("NUMEROS POSITVOS: %d%n NUMEROS NEGATIVOS: %d%n", pos, neg);
+
+        sc.close();
+    }
 }
