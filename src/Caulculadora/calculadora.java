dc65945271d425b603d98b7c3cfc4b0ea07948ec
Samuel <henrisamuel234@gmail.com>
2025-09-08 18:09:18 -0300

    Cauculadora fix

---
+++ src/Caulculadora/calculadora.java
@@ -0,0 +1,59 @@
+package Caulculadora;
+
+import java.util.Scanner;
+
+public class calculadora {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("1 PARA SOMA %n2 PARA SUBTRAIR %n3 PARA MULTIPLICAR %n4 PARA DIVIDIR %n ");
+        int opcao_User = sc.nextInt();
+        int resultado = 0;
+
+        if (opcao_User == 1) {
+            System.out.print("DIGITE UM NUMERO: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("DIGITE OUTRO NUMERO: ");
+            int num2 = sc.nextInt();
+
+            resultado = num1 + num2;
+            System.out.print(resultado);
+        }
+        else if (opcao_User == 2) {
+            System.out.print("DIGITE UM NUMERO: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("DIGITE OUTRO NUMERO: ");
+            int num2 = sc.nextInt();
+
+            resultado = num1 - num2;
+            System.out.print(resultado);
+        }
+        else if (opcao_User == 3) {
+            System.out.print("DIGITE UM NUMERO: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("DIGITE OUTRO NUMERO: ");
+            int num2 = sc.nextInt();
+
+            resultado = num1 * num2;
+            System.out.print(resultado);
+        }
+        else if (opcao_User == 4) {
+            System.out.print("DIGITE UM NUMERO: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("DIGITE OUTRO NUMERO: ");
+            int num2 = sc.nextInt();
+
+            if (num2 == 0) {
+                System.out.print("ERRO!! NÃO É DIVISIVEL POR 0");
+            }
+
+            resultado = num1 * num2;
+            System.out.print(resultado);
+
+        sc.close();
+    }
+}
