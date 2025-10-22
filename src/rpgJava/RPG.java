f0038ea72141b4c556982de35158d32a1a45474a
Samuel <henrisamuel234@gmail.com>
2025-10-22 16:55:32 -0300

    FEAT: ATributos do personagem

--- src/rpgJava/RPG.java
+++ src/rpgJava/RPG.java
@@ -17,9 +17,45 @@ public class RPG {
         System.out.print("Idade do personagem: ");
         int idade = sc.nextInt();
 
-        double vida = 100;
-        double escudo = 10;
-        double ataque = 20;
+        int vida = 100;
+        int escudo = 10;
+        int ataque = 20;
+
+        System.out.printf("%nCapitulo 2: If e elses%n");
+
+        System.out.print("Escolha sua classe");
+        System.out.printf("%n 1 - Guerreiro%n 2 - Arqueiro%n 3 - Mago%n");
+        int opcaoClasse = sc.nextInt();
+
+        if (opcaoClasse == 1) {
+            System.out.printf("%n Bem vindo %s, Voce é um guerreiro", nome);
+
+            vida += 10;
+            escudo += 10;
+            ataque += 10;
+        }
+
+        else if (opcaoClasse == 2) {
+            System.out.printf("%n Bem vindo %s, Voce é um arqueiro", nome);
+
+            vida += 5;
+            escudo -= 5;
+            ataque += 20;
+        }
+
+        else if (opcaoClasse == 3) {
+            System.out.printf("%n Bem vindo %s, Voce é um mago", nome);
+
+            vida += 20;
+            escudo -= 10;
+            ataque += 25;
+        }
+
+        else {
+            System.out.print("Voce nao escolheu nenhuma classe, ERRO");
+        }
+
+        System.out.printf("Seus Atributos: %n VIDA: %d%n ESCUDO: %d%n ATAQUE: %d%n", vida, escudo, ataque);
 
         sc.close();
     }
