961e0a79f3b619d35bdbff810d7ae6afa58e5344
Samuel <henrisamuel234@gmail.com>
2025-10-22 16:29:29 -0300

    FEAT: Menu boas vindas

--- src/rpgJava/RPG.java
+++ src/rpgJava/RPG.java
@@ -2,6 +2,8 @@ package rpgJava;
 
 public class RPG {
     public static void main(String[] args) {
-
+        System.out.println("----------------------------");
+        System.out.println("   Bem vindo ao RPG em Java  ");
+        System.out.println("----------------------------");
     }
 }
